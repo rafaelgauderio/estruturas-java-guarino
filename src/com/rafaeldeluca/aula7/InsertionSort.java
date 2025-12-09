1fb6303a4bfbd88cef018d85f4d444a18342ae48
Rafael de Luca <deluca1712@gmail.com>
2025-12-09 17:35:11 -0300

    Inserction Sort

---
+++ src/com/rafaeldeluca/aula7/InsertionSort.java
@@ -0,0 +1,62 @@
+package aula7;
+
+public class InsertionSort {
+
+	public static void main(String[] args) {
+
+		System.out.println("Um pouco mais perfomartico que o Bubble Sort.\n" + "Complexidade O(n²) no pior caso\n"
+				+ "Analogia de colocar as cartas de um barulho em ordem crescente.\n");
+
+		insertionSort(15);
+		insertionSort(100);
+		insertionSort(1000);
+		insertionSort(10000);
+		insertionSort(100000);
+
+	}
+
+	public static int[] insertionSort(int size) {
+
+		int[] vector = new int[size];
+		for (int i = 0; i < vector.length; i++) {
+			vector[i] = (int) (Math.random() * vector.length);
+		}
+		if (size < 20) {
+			System.out.println("UNSORTED vector");
+			for (int i = 0; i < vector.length; i++) {
+				System.out.print("[" + vector[i] + "] ");
+			}
+			System.out.println("\n");
+		}
+
+		long initialTime = System.currentTimeMillis(), finalTime = 0;
+		// inicia em 1 pq, vetor de uma item não precisa ordenar
+		int j, aux;
+		for (int i = 1; i < vector.length; i++) {
+			aux = vector[i];
+			j = i - 1;
+			while (j >= 0 && vector[j] > aux) {
+				vector[j + 1] = vector[j];
+				j--;
+			}
+			vector[j + 1] = aux;
+		}
+
+		finalTime = System.currentTimeMillis();
+		if (size < 20) {
+			System.out.println("\nORDERED vector");
+			for (int i = 0; i < vector.length; i++) {
+				System.out.print("[" + vector[i] + " ]");
+
+			}
+
+			System.out.println("\n");
+		}
+		System.out.println(
+				((finalTime - initialTime) + " mili seconds to order a vector with of " + size + " elements."));
+
+		return vector;
+
+	}
+
+}
