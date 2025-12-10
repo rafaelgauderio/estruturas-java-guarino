983cec8ca52bdc7395953d4cea26140421fdc7d9
Rafael de Luca <deluca1712@gmail.com>
2025-12-10 14:17:05 -0300

    insertion Sort

--- src/com/rafaeldeluca/aula7/InsertionSort.java
+++ src/com/rafaeldeluca/aula7/InsertionSort.java
@@ -4,29 +4,32 @@ public class InsertionSort {
 
 	public static void main(String[] args) {
 
-		System.out.println("Um pouco mais perfomartico que o Bubble Sort.\n" + "Complexidade O(n²) no pior caso\n"
-				+ "Analogia de colocar as cartas de um barulho em ordem crescente.\n");
+		System.out.println("Um pouco mais perfomartico que o Bubble Sort.\n"
+				+ "Analogia de colocar as cartas de um barulho em ordem crescente.\n"
+				+ "Divide a lista em uma parte ordenada e outra não ordenada, inserindo iterativamente\n"
+				+ "\tcada elemento da parte não ordenada na posição correta na parte ordenada.\n"
+				+ "Complexidade O(n²) no pior caso\n");
 
 		insertionSort(15);
 		insertionSort(100);
 		insertionSort(1000);
 		insertionSort(10000);
 		insertionSort(100000);
-
 	}
 
 	public static int[] insertionSort(int size) {
 
+		int max = 50, min = 1, range = max - min + 1;
+
 		int[] vector = new int[size];
 		for (int i = 0; i < vector.length; i++) {
-			vector[i] = (int) (Math.random() * vector.length);
+			vector[i] = (int) (Math.random() * range) + 1;
 		}
 		if (size < 20) {
 			System.out.println("UNSORTED vector");
 			for (int i = 0; i < vector.length; i++) {
 				System.out.print("[" + vector[i] + "] ");
 			}
-			System.out.println("\n");
 		}
 
 		long initialTime = System.currentTimeMillis(), finalTime = 0;
@@ -46,8 +49,7 @@ public class InsertionSort {
 		if (size < 20) {
 			System.out.println("\nORDERED vector");
 			for (int i = 0; i < vector.length; i++) {
-				System.out.print("[" + vector[i] + " ]");
-
+				System.out.print("[" + vector[i] + "] ");
 			}
 
 			System.out.println("\n");
